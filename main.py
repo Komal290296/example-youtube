d8720bcf413fe93f641f42483224d29a716d7ca0
Komal-DS-S <Komal.Sharma@DS-S.com>
2022-08-16 15:15:14 +0530

    third commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello {}! This is my second application".format(name)
+    return "Hello {}! This is my third application".format(name)
     
 
 
